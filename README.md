77440f468f5808df39060dd13548dcbba55bb6de
Ben Martin <ben@benmartinstudios.com.au>
2016-11-14 15:31:01 +1000

    Added basic support for winsound.PlaySound.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Code body should replace `import winsound` line.
 
 Supports:
 - [Beep](https://docs.python.org/3/library/winsound.html#winsound.Beep)
-- ~~[PlaySound](https://docs.python.org/2/library/winsound.html#winsound.PlaySound)~~
+- [PlaySound](https://docs.python.org/2/library/winsound.html#winsound.PlaySound) (partial)
 - ~~[MessageBeep](https://docs.python.org/2/library/winsound.html#winsound.MessageBeep)~~
 
 See [winsound-wrapper.py](winsound-wrapper.py)
--- winsound-wrapper.py
+++ winsound-wrapper.py
@@ -1,7 +1,14 @@
 """
-OS X requires sox => `brew install sox`
+Requirements:
+    Beep uses `play` command.
+    PlaySound uses `afplay` command to play files.
 
-Linux requires similar command (none tested)
+    OS X (tested):  
+        - sox => `brew install sox`
+        - afplay => included with OS X
+    Linux
+        - sox (or equivalent)
+        - afplay equivalent
 """
 
 try:
@@ -9,14 +16,27 @@ try:
 except ImportError:
     import subprocess
     class winsound:
+        SND_FILENAME = 0b00000001
+        SND_ASYNC = 0b00000010
+
         @staticmethod
         def Beep(frequency, duration):
-            subprocess.run(['/usr/local/bin/play',
+            subprocess.run(['play',
                             '--no-show-progress',
                             '--null',
                             '--channels', '1',
                             'synth', '{:f}'.format(duration/1000.0),
                             'sine', str(frequency)])
+
+        @classmethod
+        def PlaySound(cls, sound, flags):
+            if not(flags & cls.SND_FILENAME):
+                raise NotImplementedError("PlaySound wrapper only supports playing sound files.")
+
+            cmd = subprocess.Popen if flags & cls.SND_ASYNC else subprocess.run
+            cmd(['afplay', sound])
+
+
 
 if __name__ == '__main__':
     winsound.Beep(2500, 10)
